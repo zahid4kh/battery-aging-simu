62e2f98635a1a3f7c693b1828a7990f5bdf1839e
zahid4kh <halilzahid@gmail.com>
2025-07-28 19:25:30 +0200

    set minimum window size

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,17 +5,19 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import theme.AppTheme
+import java.awt.Dimension
 
 
 fun main() = application {
 
     Window(
         onCloseRequest = ::exitApplication,
-        state = rememberWindowState(size = DpSize(800.dp, 1000.dp)),
+        state = rememberWindowState(size = DpSize(800.dp, 900.dp)),
         alwaysOnTop = true,
         title = "Battery Aging Simu",
         icon = null
     ) {
+        window.minimumSize = Dimension(800, 900)
         AppTheme {
             App()
         }
