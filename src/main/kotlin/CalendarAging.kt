04b58bad26622e97c925ab6ab287ee80c1691ce2
zahid4kh <halilzahid@gmail.com>
2025-07-30 21:17:45 +0200

    TODO("continue fixing calendar aging. need more battery data to calculate pre exp factor for various SOC and T")

--- src/main/kotlin/CalendarAging.kt
+++ src/main/kotlin/CalendarAging.kt
@@ -5,7 +5,7 @@ import kotlin.math.exp
 import kotlin.math.pow
 
 class CalendarAging{
-    private val preExpFactor = 2.15f * 10f.pow(4)
+    private val preExpFactor = 2f * 10f.pow(-5)
     private val activationEnergy = 36360f
     private val gasConstant = 8.314f
     private val temperature = celsiusToKelvin(25f)
@@ -13,6 +13,24 @@ class CalendarAging{
     private val timeExponent = 0.789f // from A.Krupp table 3.4
     private val storageSOC = 0.5f
 
+    private val expTerm = exp(-activationEnergy / (gasConstant * temperature))
+    private val timeTerm = time.pow(timeExponent)
+
+    private fun calculatePreExponentialFactor(): Float{
+        // assuming Q_LOSS is ~3% for A(50%, 25°C)
+        val qLoss = 0.03f // 3%
+        val expFactor = qLoss / timeTerm
+        println("Pre Exp.Factor for A(50%_SOC, 25°C) is ~$expFactor")
+        return expFactor
+    }
+
+    fun calculateCapacityLoss(){
+        val qLoss = calculatePreExponentialFactor() * timeTerm
+        val qLossPercent = qLoss * 100f
+
+        println("Q_LOSS: $qLoss")
+        println("Capacity loss over ${hoursToDays(time)} days is $qLossPercent %")
+    }
 
     fun calculateSimpleCapacityLoss(){
         val expTerm = exp(x = -activationEnergy / (gasConstant * temperature))
@@ -21,16 +39,6 @@ class CalendarAging{
         val qLoss = preExpFactor * expTerm * time_term // Q_LOSS = 11.82% over 1 year
 
         println("Capacity loss over ${hoursToDays(time)} days is $qLoss %. Not taking SOC into account")
-    }
-
-    fun calculateCapacityLoss(){
-        val expTerm = exp(x = -activationEnergy / (gasConstant * temperature))
-        val time_term = time.pow(timeExponent)
-
-        val qLoss = preExpFactor * expTerm * time_term * storageSOC.pow(5)
-        val qLossPercent = qLoss * 100f
-
-        println("Capacity loss over ${hoursToDays(time)} days is $qLossPercent %")
     }
 
 }
