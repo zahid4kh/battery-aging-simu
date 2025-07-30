2f8ba5689d410211e786abdc22efb642184782a9
zahid4kh <halilzahid@gmail.com>
2025-07-30 19:34:09 +0200

    calendar aging calss

---
+++ src/main/kotlin/CalendarAging.kt
@@ -0,0 +1,27 @@
+import utils.celsiusToKelvin
+import utils.hoursToDays
+import utils.yearsToHours
+import kotlin.math.exp
+import kotlin.math.pow
+
+class CalendarAging{
+    // params
+    private val preExpFactor = 2.15f * 10f.pow(4)
+    private val activationEnergy = 36360f                           // J/mol
+    private val gasConstant = 8.314f                                 // J/mol
+    private val temperature = celsiusToKelvin(25f)
+    private val time = yearsToHours(1f)
+    private val timeExponent = 0.789f                               // from A.Krupp table 3.4
+    private val storageSOC = 0.5f
+
+
+    fun calculateSimpleCapacityLoss(){
+        val expTerm = exp(x = -activationEnergy / (gasConstant * temperature))
+        val time_term = time.pow(timeExponent)
+
+        val qLoss = preExpFactor * expTerm * time_term // Q_LOSS = 11.82% over 1 year
+
+        println("Capacity loss over ${hoursToDays(time)} days is $qLoss %. Not taking SOC into account")
+    }
+
+}
